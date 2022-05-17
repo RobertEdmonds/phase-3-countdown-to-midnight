ed76e2994fea46eedfab5c680c83fd1a66820a16
Robert Edmonds <bobby.edmonds89@gmail.com>
2022-05-17 16:56:44 -0600

    commit tree 9d3e7d81a3737bdd49137f171f37641b08b3458b

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
+require "pry"
 
-def countdown
+def countdown(integer=10)
+    while integer > 0
+        if integer == 0
+            "HAPPY NEW YEAR!"
+        end
+         "#{integer} SECOND(S)!"
+        integer -=1
+    end
 end
+countdown
